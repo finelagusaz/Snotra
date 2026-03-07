ea8a667470b5102be5da2248bc0cbe86a53737a8
Fine Lagusaz <algiz.rune@gmail.com>
2026-03-08 01:56:55 +0900

    chore: emptyOutDir: true (#189)

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
   build: {
     target: "esnext",
     outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "ui/main.html"),
